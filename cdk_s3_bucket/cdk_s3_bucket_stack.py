e6ba56f17a373a010ace5a08f1ee357890920c85
Sathish Jagana <120253584+Sathishjagana123@users.noreply.github.com>
2025-03-24 01:02:32 +0530

    Update cdk_s3_bucket_stack.py

--- cdk_s3_bucket/cdk_s3_bucket_stack.py
+++ cdk_s3_bucket/cdk_s3_bucket_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -10,10 +10,13 @@ class CdkS3BucketStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkS3BucketQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create S3 bucket with a specific name
+        bucket = s3.Bucket(
+            self, 
+            "MyS3Bucket",
+            bucket_name="demo-cdk-code-catalyst-sathish",  
+            encryption=s3.BucketEncryption.S3_MANAGED,  # Enable server-side encryption
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,  # Block all public access
+            removal_policy=RemovalPolicy.RETAIN,  # Retain the bucket when stack is destroyed
+            enforce_ssl=True  # Enforce SSL when accessing the bucket
+        )
